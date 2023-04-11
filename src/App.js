2f019e7075aac4b99578183b86a0e128ecb80eed
Enrique <enriquevalderrama@gmail.com>
2023-04-11 18:59:24 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyLogin from "./components/Login";
+import { Alert } from "./components/Alert";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [message, setMessage] = useState({
+    mensaje: "",
+    status: ""
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-3 mb-2 bg-dark text-white">
+      <MyLogin setMessage={setMessage} />
+      <Alert mensaje={message.mensaje} status={message.status} />
     </div>
   );
 }
-
-export default App;
